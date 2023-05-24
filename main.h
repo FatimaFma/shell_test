4ed2ebb8a9170ef1bca05d94748812cffeca3c62
FatimaFma <toomyox@gmail.com>
2023-05-24 12:45:19 -0700

    Adding new files

--- main.h
+++ main.h
@@ -1,13 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
-void execmd(char **argv);
-char *get_location(char *command);
+/* Functions */
+char **parsing_cmd(char *buff, char *buff_copy);
+void execve_cmd(char **argv);
+char *get_path(char *command);
+char *_getenv(const char *name);
+
+/* Helper Functions */
+char *_strdup(const char *str);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+
+extern char **environ;
 
 #endif
